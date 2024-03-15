843a4313587f85ced1ac6078148cefa93759e5c3
omarsleipy <109833393+omarsleipy@users.noreply.github.com>
2024-03-15 05:51:59 +0300

    Update SectionTwo.vue

--- frontend/src/components/sections/home/SectionTwo.vue
+++ frontend/src/components/sections/home/SectionTwo.vue
@@ -2,8 +2,8 @@
     
   <section class="flex flex-row items-center justify-center  ">
     <div class=" text-black  text-right p-4 rounded-lg w-[100%]">
-        <h1 class="text-2xl text-green-500">لماذا توجد شركة الصحة ؟</h1>
-        <h2 class="text-4xl">بناء نظام رعاية صحي رائد عالميا محوره الانسان</h2>
+        <h1 class="text-2xl text-green-500">لماذا توجد مُعين ؟</h1>
+        <h2 class="text-4xl">بناء نظام رعاية صحي نفسي رائد عالميا محوره الانسان</h2>
     </div>
     <CardsDefault class=""
       cardPadding="20px"
@@ -11,7 +11,7 @@
 
     >
     <p>
-        تتعلق علم النفس بدراسة السلوك والعقل البشري، ويشمل البحث في كل شيء من التطور النفسي في مرحلة الطفولة إلى الصحة النفسية.
+       رسالتنا تقديم وقاية ورعاية نفسية متكاملة يسهل الحصول عليها , بآليات آمنة ومستدامة ومبتكرة تعزز دور الرعاية الصحية النفسية.
     </p>
 
 </CardsDefault>
