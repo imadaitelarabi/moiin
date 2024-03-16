2a1d13053722b6bfe2466e512c73e9212c2fce20
omarsleipy <109833393+omarsleipy@users.noreply.github.com>
2024-03-16 06:28:28 +0300

    Update header.vue

--- frontend/src/components/layout/header.vue
+++ frontend/src/components/layout/header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flex justify-between items-center gradient-bg text-white p-4">
-    <img src="/logo.png" alt="Logo" class="h-10 w-auto" />
+    <img src="/Moeen Logo white.png" alt="Logo" class="h-10 w-auto" />
     <nav>
       <ul class="flex">
         <li class="mx-2"><a href="#">تقييمات</a></li>
